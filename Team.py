104300a40bc17a3a2d3ee6f788cd912267e20f77
prakash1895 <pbaskaran@wpi.edu>
2020-11-15 18:48:24 -0800

    Uploaded Team and Environment codes

---
+++ Team.py
@@ -0,0 +1,104 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from collections import Counter
+import random
+
+class Human(object):
+	def __init__(self):
+		self.skill = np.random.randint(3) 		# 0, 1, 2
+		self.getParam()
+
+		self.cur_wl = 0.0
+		self.cur_task_cnt = 0
+
+	def getParam(self):
+		if self.skill == 0: # Bad agent
+			self.wl_decay = random.uniform(0.8, 0.9)
+			self.task_perf = random.uniform(0.5, 0.6)
+
+		elif self.skill == 1: # Avg agent
+			self.wl_decay = random.uniform(0.7, 0.8)
+			self.task_perf = random.uniform(0.7, 0.8)
+
+		elif self.skill == 2: # Good agent
+			self.wl_decay = random.uniform(0.6, 0.7)
+			self.task_perf = random.uniform(0.9, 1.0)
+
+		else:
+			print("Invalid!")
+
+	def updateWorkload(self, n_tasks_assigned): # Update del_wl if needed 
+		del_wl = np.exp(n_tasks_assigned)
+		self.cur_wl = self.wl_decay*self.cur_wl + del_wl
+
+class Robot(object):
+	def __init__(self):
+		self.task_assigned = 0 # 0 if task is not assigned at time T; 1 if task is assigned
+		self.task_idx = 0
+
+class Team(object):
+	def __init__(self, n_teams, n_tasks, id_num):
+		skill = np.random.randint(3)
+		self.human = Human()
+		self.robot = Robot()
+		self.id = id_num
+
+		self.z_i = np.zeros(n_teams*n_tasks, dtype=int)
+		self.x_i = np.zeros(n_teams*n_tasks, dtype=int)
+		self.b_i = np.zeros(n_teams*n_tasks)
+
+	def updateZ(self, G, task_mat, n_tasks):
+		z = []
+		for j in range(len(G)):
+			if G[j] == 1:
+				z += task_mat[j]
+			else:
+				z += np.zeros(n_tasks, dtype=int)
+
+		# Sanity Check
+		if self.z_i.shape == z.shape:
+			self.z_i = z
+		else:
+			print("Invalid shape Z")
+
+	def updateB(self, task_mat, n_tasks):
+		b_i = []
+		for j in range(len(self.z)):
+			if self.z_i[j] == 1:
+				task_cnt = sum(self.x_i) - self.robot.task_assigned
+				w_oj = self.cur_wl + np.exp(task_cnt + 1)
+				a_ij = self.human.task_perf*(self.W_ol - abs(self.W_nl - w_oj))
+				p_ij = (1-self.human.task_perf)*abs(w_oj - self.cur_wl)
+				b_ij = a_ij - p_ij
+				b_i.append(b_ij)
+
+			elif self.z_i[j] == 0:
+				b_ij = 0.0
+				b_i.append(b_ij)
+
+			else:
+				print("Invalid z value in updateB")
+
+		# TODO: update the robot.task_assigned value in h_ij
+		cur_team_task = list(task_mat[self.id])
+		if self.cur_wl > self.W_nl and sum(cur_team_task) > 0: # Checking to assign task to robot agent
+			idx = cur_team_task.index(1) # find the idx of the task
+			pos = self.id*n_tasks + idx # Find the task position in z_i
+			b_i[pos] = 1.1*b_i[pos] # Alter the corresponding bidding value by 10%
+			self.watch_pos = pos
+			self.robot.task_idx = idx
+
+		self.b_i = b_i
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ environment.py
@@ -0,0 +1,64 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from collections import Counter
+import random
+from Team import Team
+
+class World(object):
+    def __init__(self, n_teams=20, n_tasks=4):
+        self.n_teams = n_teams  
+        self.n_tasks = n_tasks
+
+        self.Teams = self.generate_teams(n_teams, n_tasks)
+        self.task_mat = np.random.randint(2, size=(n_teams, n_tasks))
+        self.comm_mat = self.generate_COMM(n_teams)
+
+    def generate_teams(self, n_teams, n_tasks):
+        Teams = []
+        for i in range(n_teams):
+            T = Team(n_teams, n_tasks, i)
+            Teams.append(T)
+        return Teams
+
+    def generate_COMM(self, n_teams):
+        G = np.random.randint(2, size=(n_teams, n_teams))
+        np.fill_diagonal(G, 1)
+        comm_mat = np.tril(G) + np.tril(G, -1).T
+        return comm_mat
+
+    def generate_Z(self, comm_mat, task_mat):
+        for i in range(self.n_teams):
+            G = comm_mat[i]
+            self.Teams[i].updateZ(G, task_mat, self.n_tasks)
+
+    def generate_Z(self, task_mat):
+        for i in range(self.n_teams):
+            self.Teams[i].updateB(task_mat, self.n_tasks)
+
+    def updateWorkload(self):
+        pass
+
+    def runSimulation(self, n_episodes):
+        for i in range(n_episodes):
+            # self.comm_mat = self.generate_COMM(n_teams) # uncomment for dynamic system
+            self.task_mat = np.random.randint(2, size=(n_teams, n_tasks))
+            self.generate_Z(self.comm_mat, self.task_mat)
+            self.generate_B(self.task_mat)
+            while self.consensus != True: # Subject to change; Update the termination condition for consensus termination
+                # TODO: Phase 1 Auction
+                # TODO: Phase 2 Consensus
+
+            # Get Task performance metrics and workload metrics
+            # updateWorkload
+            
+##################################################################################################
+
+def main():
+    world_obj = World()
+    
+if __name__ == '__main__':
+    main()
+
+
+
